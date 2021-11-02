948af72e02d47c67b9e9fb47f4c7e6194c4ef2be
Ledgerleap <jin@ledgerleap.com>
2021-11-02 13:10:59 -0400

    API Updates

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -129,7 +129,7 @@ class UserController extends Controller
      */
     public function getProfile()
     {
-        $user = auth()->user()->load(['profile', 'permissions']);
+        $user = auth()->user()->load(['profile', 'permissions', 'shuftipro']);
         $user->metric = Helper::getNodeInfo($user);
         return $this->successResponse($user);
     }
