c28b94a5cfffacade7a253468b1cb85d72284909
= <=>
2022-04-21 21:04:09 -0400

    Send admins a 24 hr report of members stuck on KYC

--- app/Console/Commands/KycReport.php
+++ app/Console/Commands/KycReport.php
@@ -9,6 +9,7 @@ use App\Mail\AdminAlert;
 use App\Http\EmailerHelper;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
 
 class KycReport extends Command
 {
@@ -46,7 +47,10 @@ class KycReport extends Command
         // first do shufti temp records
         $now = Carbon::now();
         $yesterday = Carbon::now()->subHours(24);
-        $records_temp = ShuftiproTemp::where('status', 'pending')->where('created_at', '<=', $yesterday)->limit(10);
+        $records_temp = ShuftiproTemp::where('status', 'pending')
+            ->where('created_at', '<=', $yesterday)
+            ->limit(10)
+            ->get();
 
         $persons_stuck_in_pending = array();
         $known_reference_ids = array();
@@ -59,8 +63,7 @@ class KycReport extends Command
                     "name" => $person->first_name.' '.$person->last_name,
                     "email" => $person->email,
                     "shufti_reference_id" => $record->reference_id,
-                    "shufti_timestamp" => $record->created_at,
-                    "stuck_reason" => "Pending for over 24 hours. "
+                    "shufti_timestamp" => $record->created_at
                 );
 
                 $known_reference_ids[] = $record->reference_id;
@@ -68,7 +71,10 @@ class KycReport extends Command
         }
 
         // now do shuftipro denied
-        $records_pro = Shuftipro::where('status', 'denied')->where('reviewed', 0)->limit(10);
+        $records_pro = Shuftipro::where('status', 'denied')
+            ->where('reviewed', 0)
+            ->limit(10)
+            ->get();
 
         $persons_stuck_denied = array();
 
@@ -81,8 +87,7 @@ class KycReport extends Command
                         "name" => $person->first_name.' '.$person->last_name,
                         "email" => $person->email,
                         "shufti_reference_id" => $record->reference_id,
-                        "shufti_timestamp" => $record->created_at,
-                        "stuck_reason" => "Member received denied status from Shufti. Has not been review yet. "
+                        "shufti_timestamp" => $record->created_at
                     );
                 }
             }
@@ -92,26 +97,36 @@ class KycReport extends Command
         $body = "";
         $index = 1;
 
-        foreach($persons_stuck_in_pending as $p) {
-            $body .= '<b>'.(string)$index.'. </b>';
-            $body .= $p['name'].', '.$p['email'].'<br>';
-            $body .= $p['stuck_reason'].$p['shufti_timestamp'].' Shufti reference: '.$p['shufti_reference_id'].'<br><br>';
-            $index += 1;
+        if($persons_stuck_in_pending) {
+            $body .= "Members pending for over 24 hours:<br>";
+
+            foreach($persons_stuck_in_pending as $p) {
+                $body .= '<b>'.(string)$index.'. </b>';
+                $body .= $p['name'].', '.$p['email'].'<br>';
+                $index += 1;
+            }
         }
 
-        foreach($persons_stuck_denied as $p) {
-            $body .= '<b>'.(string)$index.'. </b>';
-            $body .= $p['name'].', '.$p['email'].'<br>';
-            $body .= $p['stuck_reason'].$p['shufti_timestamp'].' Shufti reference: '.$p['shufti_reference_id'].'<br><br>';
-            $index += 1;
+        $index = 1;
+
+        if($persons_stuck_denied) {
+            $body .= "<br><br>Members denied by Shufti. Not reviewed yet:<br>";
+
+            foreach($persons_stuck_denied as $p) {
+                $body .= '<b>'.(string)$index.'. </b>';
+                $body .= $p['name'].', '.$p['email'].'<br>';
+                $index += 1;
+            }
         }
 
         // compose email to admins
-        $emailerData = EmailerHelper::getEmailerData();
-        $admins = $emailerData['admins'] ?? array();
+        if($persons_stuck_in_pending || $persons_stuck_denied) {
+            $emailerData = EmailerHelper::getEmailerData();
+            $admins = $emailerData['admins'] ?? array();
 
-        if($admins) {
-            Mail::to($admins)->send(new AdminAlert('Casper Association portal KYC issues require attention', $body));
+            if($admins) {
+                Mail::to($admins)->send(new AdminAlert('Casper Association portal KYC issues require attention', $body));
+            }
         }
     }
 }
