52016719a635b73ade343256e79356d29018ea80
Ledgerleap <jin@ledgerleap.com>
2022-10-25 12:58:40 -0400

    # Node Status

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
             ->runInBackground();
         // ->withoutOverlapping();
         */
-
+        
         $schedule->command('ballot:check')
             ->everyMinute()
             ->runInBackground();
