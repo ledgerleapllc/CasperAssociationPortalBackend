bbed1f68d315d53cb3f0a8d186e961dd7ded7942
= <=>
2023-01-03 17:09:58 -0500

    check lastUpgrade is set

--- app/Console/Commands/CheckUpgrade.php
+++ app/Console/Commands/CheckUpgrade.php
@@ -10,20 +10,21 @@ use Carbon\Carbon;
 class CheckUpgrade extends Command
 {
     protected $signature = 'upgrade:check';
-	protected $description = 'Check Upgrade';
+    protected $description = 'Check Upgrade';
 
     public function __construct() {
         parent::__construct();
     }
 
     public function handle() {
-    	$lastUpgrade = Upgrade::orderBy('id', 'desc')->limit(1)->first();
+        $lastUpgrade = Upgrade::orderBy('id', 'desc')->limit(1)->first();
         if (
-        	!$lastUpgrade->reminder_24_sent &&
-        	$lastUpgrade->activation_datetime > Carbon::now('UTC') &&
-        	$lastUpgrade->activation_datetime <= Carbon::now('UTC')->addHours(24)
+            isset($lastUpgrade->reminder_24_sent) &&
+            !$lastUpgrade->reminder_24_sent &&
+            $lastUpgrade->activation_datetime > Carbon::now('UTC') &&
+            $lastUpgrade->activation_datetime <= Carbon::now('UTC')->addHours(24)
         ) {
-        	UpgradeReminder24::dispatch($lastUpgrade)->onQueue('default_long');
+            UpgradeReminder24::dispatch($lastUpgrade)->onQueue('default_long');
         }
         return 0;
     }
